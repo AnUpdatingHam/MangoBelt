9184e1037284c8a2193b834764772f1ac491fa19
ham <1319716110@qq.com>
2024-07-13 20:20:47 +0800

    增加根据红色面积判断是否有障碍功能

--- camera_main.py
+++ camera_main.py
@@ -41,7 +41,7 @@ def filter_small_components_by_bound(components, area_lower_bound):
     return valid_components.size, valid_components
 
 
-def find_extreme_points2(image, component_mask):
+def find_extreme_points(image, component_mask):
     # 步骤1：找到所有包含1的行的索引
     rows_with_ones = np.any(component_mask, axis=1)
 
@@ -84,39 +84,26 @@ def find_extreme_points2(image, component_mask):
     print(f"连通块的边界索引：最左边是 {leftmost_index}, 最右边是 {rightmost_index}")
 
 
-# def find_extreme_points(image, component_mask):
-#     # 提取连通块的像素坐标
-#     points = np.argwhere(component_mask)
-#
-#     # 找到最下最左的点
-#     min_row_index = np.argmin(points[:, 0])  # 找到行坐标中的最小值的索引
-#     bottom_left = (points[min_row_index, 0], points[min_row_index, 1])
-#
-#     # 找到最下最右的点
-#     max_col_index = np.argmax(points[:, 1])  # 找到列坐标中的最大值的索引
-#     bottom_right = (points[max_col_index, 0], points[max_col_index, 1])
-#
-#     # 计算几何中心
-#     if points.size > 0:
-#         center_of_mass = (int(np.mean(points[:, 0])), int(np.mean(points[:, 1])))
-#     else:
-#         return image  # 如果连通块为空，则直接返回原始图像
-#
-#     # 在图像上标记点
-#     cv2.circle(image, bottom_left, 5, (0, 255, 0), -1)  # 绿色：最下最左
-#     cv2.circle(image, bottom_right, 5, (255, 0, 0), -1)  # 蓝色：最下最右
-#     cv2.circle(image, center_of_mass, 5, (0, 0, 255), -1)  # 红色：几何中心
-#
-#     return bottom_left, bottom_right, center_of_mass
+def detect_obstacles(total_red_area, white_components_cnt, original_image, area_threshold=10000, white_components_threshold=10):
+    if total_red_area < area_threshold or white_components_cnt > white_components_threshold:
+        print("Obstacle detected!")
+        cv2.putText(original_image, "Obstacle detected!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    return original_image
+
+
+def judge_obstacle_by_area(image, mask):
+    height, width, channel = image.shape[:3]
+    img_size = height * width
+    bool_mask = mask.astype(bool)
+    red_pixel_count = np.sum(bool_mask)
+    print(f"imgSz = {img_size}")
+    print(f"red_pixel_count = {red_pixel_count}")
+    return (red_pixel_count / img_size) < 0.45
+
 
 def get_red(image):
     # 将图像从BGR颜色空间转换到HSV颜色空间
     hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # 定义红色在HSV空间的阈值
-    # 注意：HSV的范围通常是[0, 180] for HUE, [0, 255] for SATURATION and VALUE
-    # 下面的阈值是一个示例，您可能需要根据图像调整它们
-
     # 定义更宽泛的红色在HSV空间的阈值
     # 这里我们将色调的范围扩展到几乎整个红色区域，并降低饱和度的阈值来包含更浅的红色
     lower_red1 = np.array([0, 50, 70])
@@ -128,18 +115,14 @@ def get_red(image):
     mask1 = cv2.inRange(hsv_image, lower_red1, upper_red1)
     mask2 = cv2.inRange(hsv_image, lower_red2, upper_red2)
     mask = cv2.bitwise_or(mask1, mask2)
-
-    # 使用掩模将红色部分变为绿色
-    # 将原图像转换为BGR颜色空间
-    green = np.zeros_like(image, dtype=np.uint8)
-    green[:, :] = [0, 255, 0]  # 绿色在BGR中的表示
-
-    # image[mask != 0] = green[mask != 0]
+    if judge_obstacle_by_area(image, mask):
+        print("obstacle")
+        cv2.putText(image, "Obstacle detected!", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2,
+                    cv2.LINE_AA)
 
     components_cnt, components = find_connected_components(mask)
     valid_components_cnt, valid_components = filter_small_components_by_bound(components, area_lower_bound=50)
 
-    # 可选：可视化连通块
     # 将连通块编号绘制到原图中
     for i in valid_components:  # 遍历所有有效连通块的标签
         color = (255 * (i % 3), 255 * ((i + 1) % 3),  255 * ((i + 2) % 3))  # BGR颜色：绿色
@@ -147,19 +130,9 @@ def get_red(image):
         component_mask = (components == i)
         # 将当前连通块的像素点填充为随机颜色
         image[component_mask] = color
-
-        # 打开文件并写入矩阵，确保所有元素都转换为字符串
-        # print(component_mask)
-        # np.savetxt(f"mask{i}.txt", component_mask, delimiter=",", fmt='%d')  # 使用整数格式保存
-        # np.save(f"mask{i}.txt", component_mask)
-        # 将 NumPy 数组转换为列表，然后将其元素转换为字符串
-        # row_list = row.tolist()  # 将 NumPy 数组转换为列表
-        # row_str = ' '.join(map(str, row_list))  # 连接字符串
-        # f.write(row_str + '\n')  # 写入一行并换行
-
         # 获取偏移量
-        find_extreme_points2(image, component_mask)
-    return image
+        find_extreme_points(image, component_mask)
+    return valid_components_cnt, valid_components
 
 # 打开摄像头并实时处理视频帧
 cap = cv2.VideoCapture(0)
@@ -169,14 +142,15 @@ cap.set(cv2.CAP_PROP_FPS, 30)
 
 while True:
     try:
-        ret, frame = cap.read()
+        ret, image = cap.read()
         if not ret:
             print("Failed to grab frame")
             break
 
-        frame = get_red(frame)
+        valid_components_cnt, valid_components = get_red(image)
+
         # 显示检测结果
-        cv2.imshow("Track Detection", frame)
+        cv2.imshow("Track Detection", image)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
